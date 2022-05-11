629af7850f80e398c3c7cb6a5a222e028c1cafce
ThiagoAlexandreNeves <thiago,alexandre.ac@gmail.com>
2022-05-11 01:33:30 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/course/config/TestConfig.java
@@ -0,0 +1,33 @@
+package com.course.config;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.course.entities.User;
+import com.course.repositories.UserRepository;
+
+@Component
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	@Autowired
+	final UserRepository userRepository;
+	
+	public TestConfig(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+}
